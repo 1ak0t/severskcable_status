3fd76fc02be94a6c6def67c3728ebbe56a5aaab5
Игорь Александрович Кот <KotIA@sckg.corp>
2024-07-18 16:07:37 +0700

    Repairs by current machine

--- severskcable-pwa/src/components/machine/machine.tsx
+++ severskcable-pwa/src/components/machine/machine.tsx
@@ -1,37 +1,46 @@
-import {MachinesStatus} from "../../constants";
+import {AppRoutes, MachinesStatus} from "../../constants";
 import {Repair} from "../../types/initialState.type";
 import dayjs from "dayjs";
+import {Link} from "react-router-dom";
+import classNames from "classnames";
 
 type MachinePropsType = {
     name: string,
     status: string,
-    currentRepairDuration?: Repair;
+    currentRepairs?: Repair[],
+    id: string
 }
 
-const setMachineStatusClass = (status: string) => {
-    if (status === MachinesStatus.NotUse) {
-        return "machine machine--not-use";
-    }
-    if (status === MachinesStatus.Wrong) {
-        return "machine machine--wrong";
-    }
-    return "machine";
-}
-
-function Machine({name, status, currentRepairDuration} : MachinePropsType) {
+function Machine({name, status, currentRepairs, id} : MachinePropsType) {
     const currentTime = dayjs();
     let duration = "";
+    const machineBreaksUrl = `${AppRoutes.MachineBreaks}/${id}`;
 
-    if (currentRepairDuration !== undefined) {
-        const breakDate = dayjs(currentRepairDuration.breakDate);
+    if (currentRepairs) {
+        const currentRepairsSorted = [...currentRepairs].sort(function (repairA, repairB) {
+            if (repairA.status > repairB.status) {
+                return 1;
+            }
+            if (repairA.status < repairB.status) {
+                return -1;
+            }
+            return 0;
+        })
+        console.log(currentRepairs)
+        console.log(currentRepairsSorted)
+        const breakDate = dayjs(currentRepairsSorted[0].breakDate);
         const durationMinutes = currentTime.diff(breakDate, 'minute');
         duration = `${Math.floor(durationMinutes/60)}:${durationMinutes % 24}`;
     }
     return(
-        <article className={setMachineStatusClass(status)}>
+        <Link to={machineBreaksUrl} className={classNames(
+            'machine',
+            {'machine--not-use': status === MachinesStatus.NotUse},
+            {'machine--wrong': status === MachinesStatus.Wrong}
+        )}>
             <span className="machine__name">{name}</span>
-            {currentRepairDuration !== undefined && <span className="machine__status">{status}<br></br>{(`Длительность - ${duration}`)}</span>}
-        </article>
+            {currentRepairs && <span className="machine__status">{status}<br></br>{(`Длительность - ${duration}`)}</span>}
+        </Link>
     );
 }
 
--- severskcable-pwa/src/store/actions.ts
+++ severskcable-pwa/src/store/actions.ts
@@ -1,7 +1,8 @@
 import {createAction} from "@reduxjs/toolkit";
 import {InitialStateType} from "../types/initialState.type";
-import {CreateRepairType, NewBreakType} from "../types/types";
+import {CreateRepairType, NewBreakType, SetMachineStatusActionType} from "../types/types";
 
 export const getState = createAction<InitialStateType>('getState');
 export const setNewRepairType = createAction<CreateRepairType>('setNewRepairType');
 export const setNewBreak = createAction<NewBreakType>('setNewBreak');
+export const setMachineStatus = createAction<SetMachineStatusActionType>('setMachineStatus');
